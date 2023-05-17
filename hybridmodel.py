9de998d044a88c6b09e2cd5e06dfd88a6ce4d60a
IQRA BISMI <108056063+iqrabismii@users.noreply.github.com>
2023-05-18 02:13:33 +0530

    Update hybridmodel.py

--- hybridmodel.py
+++ hybridmodel.py
@@ -365,21 +365,20 @@ class RestaurantHybridModel:
 
         print("---------------------")
         
-        count= 1
-        name= ""
-        for i ,j in recommendations_cf.iterrows():
-         
-          if name != j['name']:
-                  print(f'{count}. ', j['name'], end=' ')
-                  
-                  print('Rating: ', j['rating_x'])
-                  
-                  print("\n")
-                  count += 1
-                  if count ==6:
-                    break
+        print("---------------------")
         
-          name = j['name']
+    
+
+        count = 1
+        for i in recommendations_cf['name'].unique():
+            print(f'{count}. ', i)
+            count = count +1
+            print("\n")
+
+            if count == 6:
+                break
+        
+        
           
           
 import sys    
